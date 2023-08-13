ee1075db02debcc02cf877c0cfe3abbce9a3c197
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2023-08-13 20:03:05 +0530

    Added the opt check in the mail

--- controller/professor/mail.js
+++ controller/professor/mail.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const otpModel = require('../../models/otp');
 const sendMail = (req, res) => {
     const email = req.body.email;
     const mailBody = req.body.mailBody;
@@ -10,23 +11,37 @@ const sendMail = (req, res) => {
             pass: 'tumaqnhvuanufppp'
         }
     });
-    var mailOptions = {
-        from: 'pratikthakur2019@gmail.com',
-        to: email,
-        subject: subject,
-        text: `${mailBody}`
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.status(200).send({
-                'message': 'Mail send'
+    const otp = req.body.otp;
+    otpModel.find({
+        email: "pratikthakur2019@gmail.com"
+    }).then((resp11) => {
+        if(resp11[0].otpNum === otp){
+            var mailOptions = {
+                from: 'pratikthakur2019@gmail.com',
+                to: email,
+                subject: subject,
+                text: `${mailBody}`
+            };
+        
+            transporter.sendMail(mailOptions, (err, info) => {
+                if(err){
+                    res.send(err);
+                }
+                else{
+                    res.status(200).send({
+                        'message': 'Mail send'
+                    });
+                }
             });
         }
-    });
+        else{
+            res.status(403).send({
+                'message': 'Wrong Otp'
+            })
+        }
+    }).catch((er11) => {
+        res.send(er11);
+    })
 }
 
 module.exports = sendMail;
